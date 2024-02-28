5da36d806dc197d4fb01ac85f4e9713a2a046fb2
pabloqpacin <110383181+pabloqpacin@users.noreply.github.com>
2024-02-28 20:31:25 +0100

    Add compose php server (AppsWeb)

---
+++ AppsWeb/server/php/src/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>TICKETARDOS</title>
+    <style>
+        body {
+            background-color: #333;
+            color: #fff;
+            font-family: system-ui, -apple-system, 'Segoe UI', Roboto, Ubuntu, sans-serif;
+            text-align: center;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body>
+    <?php
+
+
+
+    ?>
+</body>
+</html>
---
+++ AppsWeb/server/readme.md
@@ -0,0 +1,16 @@
+
+Activar server
+
+```bash
+docker compose up -d
+```
+Visitar web
+
+```bash
+xdg-open http://localhost/dev.php
+```
+Editar archivos
+
+```bash
+nvim php/src/dev.php
+```
